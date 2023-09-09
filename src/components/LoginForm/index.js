bddab624d7b7e0e57d6f764a653207a104a7dc91
pavansai <pavansaivvit@gmail.com>
2023-09-09 16:13:46 +0000

    login route completed till now

---
+++ src/components/LoginForm/index.js
@@ -0,0 +1,174 @@
+import {Component} from 'react'
+import Cookies from 'js-cookie'
+import NxtWatchContext from '../../context/NxtWatchContext'
+import {
+  AppContainer,
+  FormContainer,
+  AppLogo,
+  LabelAndInputContainer,
+  Label,
+  UsernameInput,
+  LoginButton,
+  ShowPasswordContainer,
+  ShowPasswordCheckbox,
+  ShowPasswordText,
+  ErrorMsg,
+} from './styledComponents'
+
+class LoginForm extends Component {
+  state = {
+    username: '',
+    password: '',
+    showPassword: '',
+    showErrorMsg: false,
+    errorMsg: '',
+  }
+
+  onChangeUsername = event => {
+    this.setState({username: event.target.value})
+  }
+
+  renderUsernameField = () => (
+    <NxtWatchContext.Consumer>
+      {value => {
+        const {isDarkTheme} = value
+        const {username} = this.state
+        return (
+          <LabelAndInputContainer>
+            <Label
+              htmlFor="username"
+              fontColor={isDarkTheme}
+              borderColor={isDarkTheme}
+            >
+              USERNAME
+            </Label>
+            <UsernameInput
+              id="username"
+              type="text"
+              placeholder="Username"
+              value={username}
+              onChange={this.onChangeUsername}
+              borderColor={isDarkTheme}
+            />
+          </LabelAndInputContainer>
+        )
+      }}
+    </NxtWatchContext.Consumer>
+  )
+
+  onChangePassword = event => {
+    this.setState({password: event.target.value})
+  }
+
+  renderPasswordField = () => (
+    <NxtWatchContext.Consumer>
+      {value => {
+        const {isDarkTheme} = value
+        const {password, showPassword} = this.state
+        return (
+          <LabelAndInputContainer>
+            <Label
+              htmlFor="password"
+              fontColor={isDarkTheme}
+              borderColor={isDarkTheme}
+            >
+              PASSWORD
+            </Label>
+            <UsernameInput
+              id="password"
+              type={`${showPassword ? 'text' : 'password'}`}
+              placeholder="Password"
+              value={password}
+              onChange={this.onChangePassword}
+              borderColor={isDarkTheme}
+            />
+          </LabelAndInputContainer>
+        )
+      }}
+    </NxtWatchContext.Consumer>
+  )
+
+  onClickCheckbox = event => {
+    this.setState({showPassword: event.target.checked})
+  }
+
+  renderShowPasswordField = () => (
+    <NxtWatchContext.Consumer>
+      {value => {
+        const {isDarkTheme} = value
+        return (
+          <ShowPasswordContainer>
+            <ShowPasswordCheckbox
+              type="checkbox"
+              onClick={this.onClickCheckbox}
+            />
+            <ShowPasswordText fontColor={isDarkTheme}>
+              Show Password
+            </ShowPasswordText>
+          </ShowPasswordContainer>
+        )
+      }}
+    </NxtWatchContext.Consumer>
+  )
+
+  onLoginSuccessful = jwtToken => {
+    const {history} = this.props
+    Cookies.set('jwt_token', jwtToken, {expires: 3})
+    history.replace('/')
+  }
+
+  onLoginFailure = errorMsg => {
+    this.setState({showErrorMsg: true, errorMsg})
+  }
+
+  onSubmitForm = async event => {
+    event.preventDefault()
+    const {username, password} = this.state
+    const apiUrl = 'https://apis.ccbp.in/login'
+    const userDetails = {username, password}
+    const options = {
+      method: 'POST',
+      body: JSON.stringify(userDetails),
+    }
+    const response = await fetch(apiUrl, options)
+    const data = await response.json()
+    console.log(data)
+    if (response.ok === true) {
+      this.onLoginSuccessful(data.jwt_token)
+    } else {
+      this.onLoginFailure(data.error_msg)
+    }
+  }
+
+  render() {
+    return (
+      <NxtWatchContext.Consumer>
+        {value => {
+          const {isDarkTheme} = value
+          const {showErrorMsg, errorMsg} = this.state
+          const appLogo = isDarkTheme
+            ? 'https://assets.ccbp.in/frontend/react-js/nxt-watch-logo-dark-theme-img.png'
+            : 'https://assets.ccbp.in/frontend/react-js/nxt-watch-logo-light-theme-img.png'
+          return (
+            <AppContainer bgColor={isDarkTheme}>
+              <FormContainer
+                onSubmit={this.onSubmitForm}
+                bgColor={isDarkTheme}
+                shadow={isDarkTheme}
+              >
+                <AppLogo src={appLogo} alt="website logo" />
+                {this.renderUsernameField()}
+                {this.renderPasswordField()}
+                {this.renderShowPasswordField()}
+                <LoginButton type="submit">Login</LoginButton>
+                {showErrorMsg && <ErrorMsg>*{errorMsg}</ErrorMsg>}
+              </FormContainer>
+            </AppContainer>
+          )
+        }}
+      </NxtWatchContext.Consumer>
+    )
+  }
+}
+
+export default LoginForm
---
+++ src/components/LoginForm/styledComponents.js
@@ -0,0 +1,113 @@
+import styled from 'styled-components'
+
+export const AppContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: ${props => (props.bgColor ? '#181818' : '#f9f9f9')};
+`
+export const FormContainer = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  min-width: 330px;
+  padding: 30px;
+  border-radius: 8px;
+  background-color: ${props => (props.bgColor ? '#0f0f0f' : '#f9f9f9')};
+  box-shadow: 0px 0px 10px ${props => (props.bgColor ? '#f9f9f9' : '#0f0f0f')};
+  @media screen and (min-width: 768px) {
+    width: 400px;
+    padding: 40px;
+  }
+`
+export const AppLogo = styled.img`
+  height: 40px;
+  margin-bottom: 40px;
+`
+export const LabelAndInputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 5px;
+  width: 100%;
+  margin-bottom: 10px;
+`
+export const Label = styled.label`
+  font-weight: 700;
+  font-size: 10px;
+  margin-bottom: 5px;
+  color: ${props => (props.fontColor ? '#f8fafc' : '#475569')};
+  @media screen and (min-width: 768px) {
+    font-size: 15px;
+  }
+`
+export const UsernameInput = styled.input`
+  border-radius: 4px;
+  outline: none;
+  font-size: 12px;
+  font-weight: 300;
+  padding-left: 10px;
+  height: 30px;
+  color: ${props => (props.fontColor ? '#f8fafc' : ' #1e293b')};
+  border: 1px solid ${props => (props.borderColor ? '#f8fafc' : '#94a3b8')};
+  @media screen and (min-width: 768px) {
+    font-size: 18px;
+    height: 40px;
+  }
+`
+export const LoginButton = styled.button`
+  height: 30px;
+  color: #f8fafc;
+  font-size: 12px;
+  font-weight: 500;
+  border: none;
+  outline: none;
+  background-color: #4f46e5;
+  border-radius: 6px;
+  width: 100%;
+  margin-top: 10px;
+  margin-bottom: 0px;
+  @media screen and (min-width: 768px) {
+    height: 50px;
+    font-size: 18px;
+  }
+`
+export const ShowPasswordContainer = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  margin-bottom: 10px;
+  width: 100%;
+`
+
+export const ShowPasswordCheckbox = styled.input`
+  border: 5px solid red;
+  height: 15px;
+  width: 15px;
+  margin-right: 10px;
+  @media screen and (min-width: 768px) {
+    height: 20px;
+    width: 20px;
+  }
+`
+
+export const ShowPasswordText = styled.p`
+  font-weight: 500;
+  font-size: 12px;
+  color: ${props => (props.fontColor ? '#f8fafc' : '#1e293b')};
+  @media screen and (min-width: 768px) {
+    font-size: 18px;
+  }
+`
+export const ErrorMsg = styled.p`
+  color: #ff0b37;
+  font-weight: 500;
+  font-size: 12px;
+  text-align: left;
+  width: 100%;
+  @media screen and (min-width: 768px) {
+    font-size: 18px;
+  }
+`
